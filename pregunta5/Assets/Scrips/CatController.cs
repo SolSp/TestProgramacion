5c25395c25678a8c61080ba27b3d32d2df018e15
SolSp <sol.sanchez.porto@gmail.com>
2017-08-26 21:16:41 -0500

    pregunta5 actualizacion

---
+++ pregunta5/Assets/Scrips/CatController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatController : MonoBehaviour {
+
+    Animator anim;
+    private int time;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        time=0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetInteger("State", 1);
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetInteger("State",0);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            anim.SetInteger("State", 2);
+        }
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            anim.SetInteger("State", 0);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetInteger("State", 3);
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetInteger("State", 4);
+        }
+	}
+}
